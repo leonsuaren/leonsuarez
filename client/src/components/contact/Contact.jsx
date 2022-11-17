8dade543c830c50490e2e9dcec4d12a21ce96e64
Leon Suarez <leonsua@gmail.com>
2022-11-17 10:01:23 -0500

    created contact end point and handle the form on the front end to get the contact information and message

--- client/src/components/contact/Contact.jsx
+++ client/src/components/contact/Contact.jsx
@@ -1,6 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { useFormik } from 'formik';
+import axios from 'axios';
+
+const Alert = ({ alertMessage, type }) => {
+  return (
+    <div className={`alert alert-${type} alert-style`} role="alert">
+      {alertMessage}
+    </div>
+  )
+}
 
 export const Contact = () => {
+  const [error, setError] = useState('');
+  const [submitSuccess, setSubmitSuccess] = useState(false);
+
+  const formik = useFormik({
+    validate: (values) => {
+      const errors = {};
+
+      if (!values.contactName) {
+        errors.contactName = 'Please enter a name!'
+      } else if (values.contactName.length < 4) {
+        errors.contactName = 'Please enter a valid name!'
+      }
+
+      if (!values.contactEmail) {
+        errors.contactEmail = 'Please enter an email address!';
+      } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.contactEmail)) {
+        errors.contactEmail = 'Invalid email address';
+      }
+
+      if (values.contactNumber === '') {
+        errors.contactNumber = ''
+      } else if (values.contactNumber.length > 10) {
+        errors.contactNumber = 'Invalid phone number format!'
+      } else if (values.contactNumber.length < 10) {
+        errors.contactNumber = 'Invalid phone number format!'
+      }
+
+      if (!values.contactMessage) {
+        errors.contactMessage = 'Please enter a message!'
+      }
+
+      setError(errors);
+      return errors;
+    },
+    initialValues: {
+      contactName: '',
+      contactEmail: '',
+      contactNumber: '',
+      contactMessage: ''
+    },
+    onSubmit: async values => {
+      await axios.post('http://localhost:8080/api/contacts',
+        { contactName: values.contactName, contactEmail: values.contactEmail, contactNumber: values.contactNumber, contactMessage: values.contactMessage }
+      ).then((response) => {
+        console.log(response);
+        setSubmitSuccess(true);
+        setTimeout(() => {
+          setSubmitSuccess(false);
+        }, 3000);
+      }).catch((error) => {
+        setError(error)
+      });
+    }
+  });
+
   return (
     <section className="page-section" id="contact">
       <div className="container">
@@ -19,38 +85,49 @@ export const Contact = () => {
         </div>
         <div className="row justify-content-center">
           <div className="col-lg-8 col-xl-7">
-            <form id="contactForm" data-sb-form-api-token="API_TOKEN">
+            <form id="contactForm" data-sb-form-api-token="API_TOKEN" onSubmit={formik.handleSubmit}>
               <div className="form-floating mb-3">
-                <input className="form-control" id="name" type="text" placeholder="Enter your name..." data-sb-validations="required" />
-                <label htmlFor="name">Full name</label>
-                <div className="invalid-feedback" data-sb-feedback="name:required">A name is required.</div>
+                <input className="form-control" id="contactName" type="text" placeholder="Enter your name..." data-sb-validations="required"
+                  value={formik.values.contactName}
+                  onChange={formik.handleChange}
+                />
+                <label htmlFor="name">Name</label>
+                {error.contactName ? <div className='form-field-error'>{error.contactName}</div> : ''}
+
               </div>
               <div className="form-floating mb-3">
-                <input className="form-control" id="email" type="email" placeholder="name@example.com" data-sb-validations="required,email" />
+                <input className="form-control" id="contactEmail" type="email" placeholder="name@example.com" data-sb-validations="required,email"
+                  value={formik.values.contactEmail}
+                  onChange={formik.handleChange}
+                />
                 <label htmlFor="email">Email address</label>
-                <div className="invalid-feedback" data-sb-feedback="email:required">An email is required.</div>
-                <div className="invalid-feedback" data-sb-feedback="email:email">Email is not valid.</div>
+                {error.contactEmail ? <div className='form-field-error'>{error.contactEmail}</div> : ''}
               </div>
               <div className="form-floating mb-3">
-                <input className="form-control" id="phone" type="tel" placeholder="(123) 456-7890" data-sb-validations="required" />
+                <input className="form-control" id="contactNumber" type="tel" placeholder="(123) 456-7890" data-sb-validations="required"
+                  value={formik.values.contactNumber}
+                  onChange={formik.handleChange}
+                />
                 <label htmlFor="phone">Phone number</label>
-                <div className="invalid-feedback" data-sb-feedback="phone:required">A phone number is required.</div>
+                {error.contactNumber ? <div className='form-field-error'>{error.contactNumber}</div> : ''}
               </div>
               <div className="form-floating mb-3">
-                <textarea className="form-control" id="message" type="text" placeholder="Enter your message here..." data-sb-validations="required"></textarea>
+                <textarea className="form-control" id="contactMessage" type="text" placeholder="Enter your message here..." data-sb-validations="required"
+                  value={formik.values.contactMessage}
+                  onChange={formik.handleChange}
+                ></textarea>
                 <label htmlFor="message">Message</label>
-                <div className="invalid-feedback" data-sb-feedback="message:required">A message is required.</div>
+                {error.contactMessage ? <div className='form-field-error'>{error.contactMessage}</div> : ''}
               </div>
-              <div className="d-none" id="submitSuccessMessage">
-                <div className="text-center mb-3">
-                  <div className="fw-bolder">Form submission successful!</div>
-                            To activate this form, sign up at
-                            <br />
-                  <a href="https://startbootstrap.com/solution/contact-forms">https://startbootstrap.com/solution/contact-forms</a>
-                </div>
+              <button className="btn btn-primary btn-xl" id="submitButton" type="submit">Send</button>
+              <div className='alerts-section'>
+                {
+                  submitSuccess ? <Alert alertMessage='Thanks! We will be in touch soon!' type='success' /> : ''
+                }
+                {
+                  // error ? <Alert alertMessage='Something went wrong! Please try again later!' type='danger' /> : ''
+                }
               </div>
-              <div className="d-none" id="submitErrorMessage"><div className="text-center text-danger mb-3">Error sending message!</div></div>
-              <button className="btn btn-primary btn-xl disabled" id="submitButton" type="submit">Send</button>
             </form>
           </div>
         </div>
--- models/contact.js
+++ models/contact.js
@@ -8,7 +8,6 @@ const ContactSchema = new mongoose.Schema({
   contactEmail: {
     type: String,
     require: true,
-    unique: true,
     match: [
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
       "Please provide a valid email"
