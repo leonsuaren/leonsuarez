a07b4bfcba3a50b5fdb1ab60fd2a2c485e59fff3
Leon Suarez <leonsua@gmail.com>
2022-11-19 17:25:39 -0500

    login form done

--- client/src/views/login-as-admin/LoginAsAdmin.jsx
+++ client/src/views/login-as-admin/LoginAsAdmin.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export const LoginAsAdmin = () => {
   return (
     <div className='login-as-admin-body'>
       <div className='login-as-admin-window'>
-        hola
+        <div className='login-as-admin-inner-div'>
+          <div className='login-as-admin-wrapper'>
+            <h5>Login as Admin</h5>
+            <form className='login-as-admin-form'>
+              <div className="login-as-admin-input">
+                <label htmlFor="adminName">Admin</label>
+                <input className="login-input" id="adminName" type="password" data-sb-validations="required"
+                />
+              </div>
+              <div className="login-as-admin-input">
+                <label htmlFor="adminPassword">Password</label>
+                <input className="login-input" id="adminPassword" type="password" data-sb-validations="required"
+                />
+              </div>
+              <div className='login-as-admin-form-buttons'>
+                <button className='btn btn-lg btn-outline-light'>LOGIN</button>
+                <Link className='btn btn-lg btn-outline-light' to='/'>CANCEL</Link>
+              </div>
+            </form>
+          </div>
+        </div>
       </div>
     </div>
   )
