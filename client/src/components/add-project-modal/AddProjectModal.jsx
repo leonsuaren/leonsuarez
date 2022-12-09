7a640f80a54f441285c2f338abee685137c3bd2e
Leon Suarez <leonsua@gmail.com>
2022-12-08 19:03:51 -0500

    handle create a project form all completed and working

--- client/src/components/add-project-modal/AddProjectModal.jsx
+++ client/src/components/add-project-modal/AddProjectModal.jsx
@@ -7,7 +7,7 @@ import { Loading } from '../loading';
 const Alert = ({ message, alertType }) => {
   return (
     <div className={`alert alert-${alertType}`} role="alert">
-      { message }
+      {message}
     </div>
   )
 }
@@ -63,6 +63,7 @@ export const AddProjectModal = () => {
         }, 3000);
         setTimeout(() => {
           setMessage('');
+          formik.resetForm();
         }, 6000);
       }).catch((error) => {
         setMessage(error.response.data.message);
@@ -71,17 +72,17 @@ export const AddProjectModal = () => {
     }
   });
 
-  console.log(message);
+  console.log(message, alert);
 
   const handleOnCancelCreateProject = () => {
-
+    formik.resetForm();
   }
 
   return (
     <div className="portfolio-modal modal fade" id="addProjectModal" tabIndex="-1" aria-labelledby="addProjectModal" aria-hidden="true">
       <div className="modal-dialog modal-xl">
         <div className="modal-content">
-          <div className="modal-header border-0"><button className="btn-close" type="button" data-bs-dismiss="modal" aria-label="Close"></button></div>
+          <div className="modal-header border-0"><button className="btn-close" type="button" data-bs-dismiss="modal" aria-label="Close" onClick={handleOnCancelCreateProject}></button></div>
           <div className="modal-body text-center pb-5">
             <div className="container">
               <div className="row justify-content-center">
@@ -95,59 +96,64 @@ export const AddProjectModal = () => {
                     <div className="divider-custom-line"></div>
                   </div>
                   {
-                    message.length > 0 ? 
-                    <div>
-                      {
-                        loading ? <Loading /> : <div><Alert message={message} alertType={alert}/></div>
-                      }
-                    </div> : ''
+                    message.length > 0 ?
+                      <div>
+                        {
+                          loading ? <Loading /> : <div><Alert message={message} alertType={alert} /></div>
+                        }
+                      </div> : ''
                   }
                   <div className="modal-body">
                     <form onSubmit={formik.handleSubmit}>
                       <div className="form-floating mb-3">
-                        <input className="form-control capitalized" id="projectName" type="text" placeholder="Enter your name..." data-sb-validations="required"
-                          value={formik.values.projectName}
+                        <input className="form-control capitalized" id="projectName" name='projectName' type="text" placeholder="Enter your name..." data-sb-validations="required"
                           onChange={formik.handleChange}
+                          onBlur={formik.handleBlur}
+                          value={formik.values.projectName}
                           disabled={loading}
                         />
                         <label htmlFor="name">Project Name</label>
-                        {formik.errors.projectName ? <div className='form-field-error'>{formik.errors.projectName}</div> : ''}
+                        {formik.touched.projectName && formik.errors.projectName ? <div className='form-field-error'>{formik.errors.projectName}</div> : null}
                       </div>
                       <div className="form-floating mb-3">
                         <input className="form-control capitalized" id="projectAutor" type="text" placeholder="Enter your name..." data-sb-validations="required"
-                          value={formik.values.projectAutor}
                           onChange={formik.handleChange}
+                          onBlur={formik.handleBlur}
+                          value={formik.values.projectAutor}
                           disabled={loading}
                         />
                         <label htmlFor="name">Autor</label>
-                        {formik.errors.projectAutor ? <div className='form-field-error'>{formik.errors.projectAutor}</div> : ''}
+                        {formik.touched.projectAutor && formik.errors.projectAutor ? <div className='form-field-error'>{formik.errors.projectAutor}</div> : null}
                       </div>
                       <div className="form-floating mb-3">
                         <input className="form-control" id="projectRepo" type="text" placeholder="Enter your name..." data-sb-validations="required"
-                          value={formik.values.projectRepo}
                           onChange={formik.handleChange}
+                          onBlur={formik.handleBlur}
+                          value={formik.values.projectRepo}
                           disabled={loading}
                         />
                         <label htmlFor="name">Repository</label>
-                        {formik.errors.projectRepo ? <div className='form-field-error'>{formik.errors.projectRepo}</div> : ''}
+                        {formik.touched.projectRepo && formik.errors.projectRepo ? <div className='form-field-error'>{formik.errors.projectRepo}</div> : null}
                       </div>
                       <div className="form-floating mb-3">
                         <input className="form-control" id="projectWebsite" type="text" placeholder="Enter your name..." data-sb-validations="required"
-                          value={formik.values.projectWebsite}
                           onChange={formik.handleChange}
+                          onBlur={formik.handleBlur}
+                          value={formik.values.projectWebsite}
                           disabled={loading}
                         />
                         <label htmlFor="name">Website</label>
-                        {formik.errors.projectWebsite ? <div className='form-field-error'>{formik.errors.projectWebsite}</div> : ''}
+                        {formik.touched.projectWebsite && formik.errors.projectWebsite ? <div className='form-field-error'>{formik.errors.projectWebsite}</div> : null}
                       </div>
                       <div className="form-floating mb-3">
                         <textarea className="form-control" id="projectDescription" type="text" placeholder="Enter your name..." data-sb-validations="required"
-                          value={formik.values.projectDescription}
                           onChange={formik.handleChange}
+                          onBlur={formik.handleBlur}
+                          value={formik.values.projectDescription}
                           disabled={loading}
                         />
                         <label htmlFor="name">Description</label>
-                        {formik.errors.projectDescription ? <div className='form-field-error'>{formik.errors.projectDescription}</div> : ''}
+                        {formik.touched.projectDescription && formik.errors.projectDescription ? <div className='form-field-error'>{formik.errors.projectDescription}</div> : null}
                       </div>
                       <div className="form-floating mb-3">
                         <button className="form-control" id="projectImage" type="file" data-sb-validations="required">Select an Image</button>
@@ -155,7 +161,7 @@ export const AddProjectModal = () => {
                         }
                       </div>
                       <div>
-                        <button className="btn btn-danger button-margin" type="button" data-bs-dismiss="modal" aria-label="Close" disabled={loading}
+                        <button className="btn btn-danger button-margin" type="reset" data-bs-dismiss="modal" aria-label="Close" disabled={loading}
                           onClick={handleOnCancelCreateProject}
                         >
                           Cancel
